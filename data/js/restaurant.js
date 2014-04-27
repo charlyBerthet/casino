16cd512021ff572538e76e9b589bd669ded4c519
charlyBerthet <charlyberthet@orange.fr>
2014-04-27 18:10:22 +0200

    debug

--- data/js/restaurant.js
+++ data/js/restaurant.js
@@ -28,9 +28,9 @@ $(function(){
 			$("#panoRestaurantSWF").attr("src","http://www.casino-megeve.com/restaurant-l-equipe/vtour/tour.swf");
 		}else{ */// SINON on prend ce qu'on a en local
 		
-			$("#panoRestaurantSWF").attr("flashvars","browser.useragent="+navigator.userAgent+"&xml=data/pano/restaurant/tour.xml");
+			//$("#panoRestaurantSWF").attr("flashvars","browser.useragent="+navigator.userAgent+"&xml=data/pano/restaurant/tour.xml");
 			$("#panoRestaurantSWF").attr("src","data/pano/restaurant/tour.swf");
-			alert($("#panoRestaurantSWF").attr("flashvars"));
+			//alert($("#panoRestaurantSWF").attr("flashvars"));
 		//}
 	};
 	
